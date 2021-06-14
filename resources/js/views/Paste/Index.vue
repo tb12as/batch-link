1fa7c91039e7c29375169438512ba186107a4f67
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2021-06-14 22:07:11 +0700

    paste index view update

--- resources/js/views/Paste/Index.vue
+++ resources/js/views/Paste/Index.vue
@@ -1,21 +1,55 @@
 <template>
   <div class="columns">
-    <div class="column m-1">
-        {{ pastes }}
+    <div class="column m-1 mt-4">
+      <div class="card">
+        <div class="card-header">
+          <p class="card-header-title">Your Paste</p>
+        </div>
+
+        <div class="card-content">
+          <div class="content">
+            <div class="table-container">
+              <table class="table is-bordered is-striped is-hoverable is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Paste Title</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr v-for="(paste, i) in pastes" :key="i">
+                    <td>{{ i+1 }}</td>
+                    <td>{{ paste.title }}</td>
+                    <td>
+                      <button class="button is-danger is-small m-1">Delete</button>
+                      <router-link
+                        class="button is-link is-small m-1"
+                        :to="{name: 'paste.show', params: {slug: paste.slug}}"
+                      >Detail</router-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      pastes: []
-    };
+  mounted() {
+    this.$store.dispatch("paste/get");
   },
 
-  mounted() {
-    axios.get("/api/paste?with-links=true").then(res => (this.pastes = res.data.pastes));
+  computed: {
+    pastes() {
+      return this.$store.state.paste.pastes;
+    }
   }
 };
 </script>
