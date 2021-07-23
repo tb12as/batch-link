f73b57f409d1447275b796329999f9ee6d51f738
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2021-07-23 18:03:14 +0700

    send verification email using queue

---
+++ app/Listeners/SendEmailVerificationCustom.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+
+
+class SendEmailVerificationCustom extends SendEmailVerificationNotification implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        parent::handle($event);
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
+use App\Listeners\SendEmailVerificationCustom;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+// use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
@@ -16,7 +17,8 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
+            // SendEmailVerificationNotification::class,
+            SendEmailVerificationCustom::class,
         ],
     ];
 
